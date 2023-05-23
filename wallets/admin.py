9c6f2eae9c95a5a5e0ae716e2dae807a882388ab
Mature Lion <maturelion1@gmail.com>
2023-05-23 12:21:13 +0100

    all currency plus signal

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from .models import Wallet
+from .models import Wallet, Currency, CurrencyBalance
 from import_export.admin import ImportExportModelAdmin
+
+
+class CurrencyAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["name", "symbol"]
+
 
 
 class WalletAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ["user__username"]
-    list_display = ["user", "balance"]
+    list_display = ["user"]
+
+class CurrencyBalanceAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    # search_fields = ["user__username"]
+    list_display = [
+        "wallet",
+        "currency",
+        "balance",
+    ]
 
 
+admin.site.register(Currency, CurrencyAdmin)
+admin.site.register(CurrencyBalance, CurrencyBalanceAdmin)
 admin.site.register(Wallet, WalletAdmin)
--- wallets/models.py
+++ wallets/models.py
@@ -5,14 +5,39 @@ from users.models import User
 
 
 @receiver(post_save, sender=User)
-def wallet_create(sender, instance=None, created=False, **kwargs):
+def create_user_wallet(sender, instance, created, **kwargs):
     if created:
-        Wallet.objects.create(user=instance, balance=0)
+        # Create a wallet for the user
+        wallet = Wallet.objects.create(user=instance)
 
+        # Get all available currencies
+        currencies = Currency.objects.all()
 
-class Wallet(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user")
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+        # Create currency balances for each currency
+        for currency in currencies:
+            CurrencyBalance.objects.create(wallet=wallet, currency=currency, balance=0.0)
+
+class Currency(models.Model):
+    name = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.user.username
+        return self.name
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    currencies = models.ManyToManyField(Currency, through='CurrencyBalance')
+
+    def __str__(self):
+        return f"{self.user.username}'s Wallet"
+
+class CurrencyBalance(models.Model):
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+
+    class Meta:
+        unique_together = ('wallet', 'currency')
+
+    def __str__(self):
+        return f"{self.currency.symbol} balance: {self.balance}"
